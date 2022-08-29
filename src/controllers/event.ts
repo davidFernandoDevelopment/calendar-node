c7c3b9025e2192ff2f8fb2ddb721d551ff44b9e0
David Sanchez <fernandoge210895@gmail.com>
2022-08-29 09:16:00 -0500

    Persistiendo el _id del cliente

--- src/controllers/event.ts
+++ src/controllers/event.ts
@@ -3,79 +3,87 @@ import { Request, Response } from 'express';
 import EventModel from '../models/Event';
 
 export const getEvents = async (req: Request, res: Response) => {
-    const evts = await EventModel
-        .find({})
-        .populate('user', 'name');
+	const evts = await EventModel.find({}).populate('user', 'name');
 
-    res.status(200).json({
-        ok: true,
-        eventos: evts
-    });
+	res.status(200).json({
+		ok: true,
+		eventos: evts,
+	});
 };
 
 export const createEvent = async (req: Request, res: Response) => {
-    const evt = new EventModel(req.body);
+	const evt = new EventModel(req.body);
 
-    try {
-        evt.user = req.uid;
-        const eventoDB = await evt.save();
-        return res.status(200).json({
-            ok: true,
-            evento: eventoDB
-        });
-    } catch (err) {
-        res.status(500).json({ ok: false, msg: 'Error en el servidor' });
-    }
+	try {
+		evt.user = req.uid;
+		if (evt.id) evt._id = evt.id;
+		const eventoDB = await evt.save();
+		return res.status(200).json({
+			ok: true,
+			evento: eventoDB,
+		});
+	} catch (err) {
+		res.status(500).json({ ok: false, msg: 'Error en el servidor' });
+	}
 };
 
 export const deleteEvent = async (req: Request, res: Response) => {
-    const {
-        uid,
-        params: { id }
-    } = req;
-    try {
-        const evt = await EventModel.findById(id);
-        if (!evt)
-            return res.status(404).json({ ok: false, msg: 'No existe el evento' });
+	const {
+		uid,
+		params: { id },
+	} = req;
+	try {
+		const evt = await EventModel.findById(id);
+		if (!evt)
+			return res.status(404).json({ ok: false, msg: 'No existe el evento' });
 
-        if (evt.user.toString() !== uid)
-            return res.status(401).json({ ok: false, msg: 'No puedes eliminar el evento de otra persona' });
+		if (evt.user.toString() !== uid)
+			return res
+				.status(401)
+				.json({
+					ok: false,
+					msg: 'No puedes eliminar el evento de otra persona',
+				});
 
-        const evtDeleted = await EventModel.findByIdAndDelete(id, { new: true });
+		const evtDeleted = await EventModel.findByIdAndDelete(id, { new: true });
 
-        res.status(200).json({
-            ok: true,
-            evento: evtDeleted
-        });
-    } catch (err) {
-        res.status(500).json({ ok: false, msg: 'Error en el servidor' });
-    }
+		res.status(200).json({
+			ok: true,
+			evento: evtDeleted,
+		});
+	} catch (err) {
+		res.status(500).json({ ok: false, msg: 'Error en el servidor' });
+	}
 };
 
 export const updateEvent = async (req: Request, res: Response) => {
-    const {
-        uid,
-        params: { id }
-    } = req;
-    try {
-        const evt = await EventModel.findById(id);
-        if (!evt)
-            return res.status(404).json({ ok: false, msg: 'No existe el evento' });
+	const {
+		uid,
+		params: { id },
+	} = req;
+	try {
+		const evt = await EventModel.findById(id);
+		if (!evt)
+			return res.status(404).json({ ok: false, msg: 'No existe el evento' });
 
-        if (evt.user.toString() !== uid)
-            return res.status(401).json({ ok: false, msg: 'No puedes editar el evento de otra persona' });
+		if (evt.user.toString() !== uid)
+			return res
+				.status(401)
+				.json({ ok: false, msg: 'No puedes editar el evento de otra persona' });
 
-        const newEvt = {
-            ...req.body,
-            user: uid
-        };
-        const evtUpdated = await EventModel.findByIdAndUpdate(id, newEvt, { new: true });
+		const newEvt = {
+			...req.body,
+			user: uid,
+		};
+		const evtUpdated = await EventModel.findByIdAndUpdate(id, newEvt, {
+			new: true,
+		});
 
-        res.status(200).json({
-            ok: true,
-            evento: evtUpdated
-        });
-    } catch (err) {
-        res.status(500).json({ ok: false, msg: 'Error en el servidor' });
-    }
+		res.status(200).json({
+			ok: true,
+			evento: evtUpdated,
+		});
+	} catch (err) {
+		res.status(500).json({ ok: false, msg: 'Error en el servidor' });
+	}
 };
